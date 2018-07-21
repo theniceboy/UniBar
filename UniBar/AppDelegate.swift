b9c6b7b0910c5d068ce7dbc1832c7973c612398a
David Chen <chenwei2000121@163.com>
2018-07-21 23:37:53 +0800

    Initial Commit

---
+++ UniBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  UniBar
+//
+//  Created by David Chen on 7/21/18.
+//  Copyright © 2018 David Chen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ UniBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  UniBar
+//
+//  Created by David Chen on 7/21/18.
+//  Copyright © 2018 David Chen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
